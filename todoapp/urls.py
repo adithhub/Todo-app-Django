cb4977754babbb89939505586835c3eca1f042c9
Adith <adithyancb3@gmail.com>
2024-05-28 20:57:37 +0530

    In this web-app now wecan update within the frontend

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.task_view,name="task_view"),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('update/<int:task_id>/', views.update_task, name='update_task'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
+from .forms import TaskForm
 
 def task_view(request):
     obj1 = Task.objects.all()
     if request.method == 'POST':
-        name = request.POST.get('name')
-        desc = request.POST.get('desc')
-        priority = request.POST.get('priority')
-        date = request.POST.get('date')
-        if name and desc and priority:
-            obj = Task(name=name, desc=desc, priority=priority, date=date)
-            obj.save()
-    return render(request, 'task_view.html', {'obj1': obj1})
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_view')
+    else:
+        form = TaskForm()
+    return render(request, 'task_view.html', {'form': form, 'obj1': obj1})
 
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     if request.method == 'POST':
         task.delete()
-        return redirect('task_view')  # Assuming 'task_view' is the name of the URL pattern for your task list view
+        return redirect('task_view')
     return render(request, 'delete_task.html', {'task': task})
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'update_task.html', {'form': form, 'task': task})
